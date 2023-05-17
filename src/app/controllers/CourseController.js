96eda900861fb0cbb30d6634c7f69e909a3bf4f3
Duy Tran <duy88tran@mail.com>
2023-05-17 12:11:27 +0700

    create new course + use mongoose-slug-updater to create slug

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,6 +10,21 @@ class CourseController {
       )
       .catch(next);
   }
+
+  //[GET] /course/create
+  create(req, res, next) {
+    res.render("course/create");
+  }
+
+  //[POST] /course/store
+  store(req, res, next) {
+    const formData = { ...req.body };
+    formData.image = `https://img.youtube.com/vi/${formData.videoId}/sddefault.jpg`;
+
+    const course = new Course(formData);
+
+    course.save().then(res.redirect("/")).catch(next);
+  }
 }
 
 module.exports = new CourseController();
